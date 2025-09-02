868a698eb0af46519d2ebc450c52a9613ddb71ed
Mansi Bellani <90083434+MansiBellani@users.noreply.github.com>
2025-09-01 19:56:50 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ report_generator = ReportGenerator()
 origins = [
     "http://localhost",
     "http://localhost:5176",
-    "https://fed-landscape-tcwb.vercel.app/"
+    "https://fed-landscape-tcwb.vercel.app"
 ]
 
 app.add_middleware(
