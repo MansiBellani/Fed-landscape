8bde12abe3db4da8686091ee34f13702b1b6e0fa
Mansi Bellani <90083434+MansiBellani@users.noreply.github.com>
2025-09-01 19:07:35 -0700

    Delete backend/download_model.py

--- backend/download_model.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-# This line will download and cache the model.
-SentenceTransformer("all-MiniLM-L6-v2")
-
-print("✅ Model downloaded and cached successfully.")
